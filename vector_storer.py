fbb774d39ce112111a4aa4188f5b41f250c5ded2
Liubomyr Midzhak <lmidzhak@gmail.com>
2025-06-05 14:43:17 +0300

    Refactored module name

--- vector_storer.py
+++ vector_storer.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
 from parser import load_documents
-from image_embedder import get_text_embedding
+from text_embedder import get_text_embedding
 from dotenv import load_dotenv
 
 from PIL import Image
