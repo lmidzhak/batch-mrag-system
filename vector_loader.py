d9f7454ca46564a2a6547cf22377ca52dc3864d8
Liubomyr Midzhak <lmidzhak@gmail.com>
2025-06-05 14:34:53 +0300

    Added load_image_vectorstore to vector_loader

--- vector_loader.py
+++ vector_loader.py
@@ -1,11 +1,27 @@
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
+import faiss
+import joblib
+import os
 
 
-def load_vectorstore():
+def load_text_vectorstore():
     embedder = OpenAIEmbeddings()
     return FAISS.load_local(
         "vectorstore/faiss_openai",
         embeddings=embedder,
         allow_dangerous_deserialization=True
     )
+
+
+def load_image_vectorstore():
+    index_path = "vectorstore/faiss_images.index"
+    metadata_path = "vectorstore/image_metadata.pkl"
+
+    if not (os.path.exists(index_path) and os.path.exists(metadata_path)):
+        raise FileNotFoundError("Image vectorstore files are missing.")
+
+    index = faiss.read_index(index_path)
+    metadata = joblib.load(metadata_path)
+
+    return index, metadata
